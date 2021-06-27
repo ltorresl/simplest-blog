17140db1a47cd06f29f1f3bba1238000e26d7957
Lucas Leite <lucasleite91@gmail.com>
2021-06-27 15:06:12 +0100

    added security for the posts inside the authors view and controller and also added nesting to the routes file with scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :authors
   get 'home/index'
+
   # Setup root page for Devise gem
   root to: "home#index"
+  scope module: 'authors' do
+    resources :posts
+  end
 end
