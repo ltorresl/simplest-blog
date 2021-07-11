1c8d5709f6639bbd970fc77442c921b797ba3570
Lucas Leite <lucasleite91@gmail.com>
2021-07-11 17:13:34 +0400

    started work on the reader pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,14 @@ class Post < ApplicationRecord
   has_many :elements
   has_one_attached :header_image
 
-  validates_presence_of :title, :description
+  validates_presence_of :title, :description, :header_image
   validates_length_of :description, within: 50..250
+
+  scope :published, -> do
+    where(published: true)
+  end
+
+  scope :most_recently_published, -> do
+    order(published_at: :desc)
+  end
 end
